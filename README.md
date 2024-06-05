f80a5839816cbf1262bfe60ee32ea65e6923cb55
Mills888dev <mills88812@gmail.com>
2024-06-05 08:52:19 -0500

    Init

---
+++ README.md
@@ -0,0 +1,6 @@
+Links:
+- [Modding Wiki](https://rainworldmodding.miraheze.org/wiki/Downpour_Reference/Mod_Directories) for `modinfo.json` documentation.
+
+
+
+Use this as a unevirsal modding template designed to be used alongside Visual Studio requiring just pressing CTRL+B to build the project.
---
+++ src/Plugin.cs
@@ -0,0 +1,44 @@
+﻿using BepInEx;
+using Fisobs.Core;
+using Menu.Remix.MixedUI;
+using MonoMod.RuntimeDetour;
+using RWCustom;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Security;
+using System.Security.Permissions;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+
+#pragma warning disable CS0618 // Type or member is obsolete
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+[module: UnverifiableCode]
+#pragma warning restore CS0618 // Type or member is obsolete
+namespace FriendlyLizard
+{
+    [BepInPlugin("thelost", "The Lost", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+
+	/*Put plugins here
+		Press CTRL+B to build project and all files will automaticly place themselves within the mod folder within the directory.*/
+        public void OnEnable()
+        {
+            Content.Register(new FriendlyLizardcritob());
+            
+       
+         
+         
+
+        }
+
+
+
+
+     
+ 
+    }   
+}
